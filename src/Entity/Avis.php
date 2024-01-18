1a8f4671fbe8d8221ce123f710317eb50e6edb4f
Sebastien35 <Sborgne@live.fr>
2024-01-18 15:05:01 +0100

    WIP

--- src/Entity/Avis.php
+++ src/Entity/Avis.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\AvisRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: AvisRepository::class)]
 class Avis
@@ -20,6 +21,7 @@ class Avis
     private ?string $Avis_content = null;
 
     #[ORM\Column]
+    #[Assert\Range(min: 0, max: 5)]
     private ?int $note = null;
 
     #[ORM\Column]
