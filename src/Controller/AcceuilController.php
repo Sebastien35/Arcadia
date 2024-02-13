72369989567a4fc09fc83c8a2c06ec32d361e794
Sebastien35 <Sborgne@live.fr>
2024-02-14 00:42:49 +0100

    Update navbar style and add Avis functionality

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -5,12 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Avis;
+use App\Repository\AvisRepository;
+
+
+
 
 class AcceuilController extends AbstractController
 {
-    #[Route('/', name: 'app_acceuil')]
-    public function index(): Response
+    public function __construct(AvisRepository $avisRepository)
     {
+        $this->AvisRepository = $avisRepository;
+    }
+
+
+    #[Route('/', name: 'app_acceuil')]
+    public function index(AvisRepository $avisRepository): Response
+    {
+
+        $avis = $this->AvisRepository->findAll();
         return $this->render('acceuil/index.html.twig', [
             'controller_name' => 'AcceuilController',
         ]);
