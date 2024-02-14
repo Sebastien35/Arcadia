59d303221a9553135637a723f19c4ece672593a1
Sebastien35 <Sborgne@live.fr>
2024-02-14 17:44:56 +0100

    Update navbar and footer styles, add new functionality for creating habitats, and fix bugs

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -6,16 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AvisRepository;
+use App\Repository\HoraireRepository;
 
 class AcceuilController extends AbstractController
 {
     #[Route('/', name: 'app_acceuil')]
-    public function index(AvisRepository $avisRepository): Response
+    public function index(AvisRepository $avisRepository, HoraireRepository $horaireRepository): Response
     {
         $avis = $avisRepository->findAll();
+        $horaires = $horaireRepository->findAll();
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AcceuilController',
-            'avis' => $avis
+            'avis' => $avis,
+            'horaires' => $horaires,
         ]);
     }
 }
