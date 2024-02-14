3e7df2cec35db5dd14f53b54826c56f2c4b84a64
Sebastien35 <Sborgne@live.fr>
2024-02-14 13:19:02 +0100

    Fix navbar styling and update homepage template

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -5,27 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Avis;
 use App\Repository\AvisRepository;
-
-
-
 
 class AcceuilController extends AbstractController
 {
-    public function __construct(AvisRepository $avisRepository)
-    {
-        $this->AvisRepository = $avisRepository;
-    }
-
-
     #[Route('/', name: 'app_acceuil')]
     public function index(AvisRepository $avisRepository): Response
     {
-
-        $avis = $this->AvisRepository->findAll();
-        return $this->render('acceuil/index.html.twig', [
+        $avis = $avisRepository->findAll();
+        return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AcceuilController',
+            'avis' => $avis
         ]);
     }
 }
