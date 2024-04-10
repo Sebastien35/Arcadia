81012b6810639c962cd88f1dd0a998287e3f9da4
Sebastien35 <Sborgne@live.fr>
2024-04-10 18:41:50 +0200

    Add star rating functionality to avis.js and update CSS styles in avis.css

--- public/js/avis.js
+++ public/js/avis.js
@@ -63,7 +63,26 @@ async function deleteAvis() {
 
 
 
+let starRating = document.querySelector('.starRating');
+let stars = document.querySelectorAll('.star');
 
+starRating.addEventListener('input', function() {
+    let rating = parseInt(this.value);
+    console.log(rating);
+    updateStarColors(rating);
+});
+
+
+function updateStarColors(rating) {
+    stars.forEach(star => {
+        let starValue = parseInt(star.dataset.value);
+        if (starValue <= rating) {
+            star.classList.add('active');
+        } else {
+            star.classList.remove('active');
+        }
+    });
+}
 
 
 /*************************************************************************/
