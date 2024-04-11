ec1d43384bc347474a27d1cfb696176d51c72c1c
Sebastien35 <Sborgne@live.fr>
2024-04-11 11:31:28 +0200

    Update star rating functionality and form validation in avis.js and AvisType.php

--- public/js/avis.js
+++ public/js/avis.js
@@ -1,5 +1,4 @@
-const pseudoInput = document.getElementById('pseudoInput');
-const noteInput = document.getElementById('noteInput');
+const pseudoInput = document.getElementById('avis_pseudo');
 const avisContentInput = document.getElementById('avisContentInput');
 
 
@@ -68,10 +67,8 @@ let stars = document.querySelectorAll('.star');
 
 starRating.addEventListener('input', function() {
     let rating = parseInt(this.value);
-    console.log(rating);
     updateStarColors(rating);
 });
-
 
 function updateStarColors(rating) {
     stars.forEach(star => {
@@ -83,6 +80,79 @@ function updateStarColors(rating) {
         }
     });
 }
+
+const avisInput = document.querySelector('.avisInput');
+const avisLen = document.getElementById('avisLen');
+const btnSendReview = document.querySelector('.btnSendReview');
+avisInput.addEventListener('input', function() {
+    let avisContent = this.value;
+    avisLen.innerHTML = `${avisContent.length}/255`;
+    validationLenAvis_Content();
+});
+
+
+
+
+/* Validation formulaire */
+function validationLenAvis_Content(){
+    let avisContent = avisInput.value;
+    if(avisContent.length < 10 || avisContent.length > 255){
+        avisInput.classList.add('is-invalid');
+        avisLen.innerHTML= `Dites nous en plus...`;
+        avisLen.classList.add('text-danger');
+        return false;
+    } else {
+        avisInput.classList.remove('is-invalid');
+        avisInput.classList.add('is-valid');
+        return true;
+    }
+}
+
+const noteInput = document.getElementById('avis_note')
+noteInput.addEventListener('input', function() {
+    validationNote();
+});
+document.addEventListener('DOMContentLoaded', function() {
+    validationNote();
+    validateForm();
+});
+function validationNote(){
+    let note = noteInput.value;
+    if(note == null || note == 0 || note == undefined || note == ''){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+pseudoInput.addEventListener('input', function() {
+    validationLenPseudo();
+});
+
+function validationLenPseudo(){
+    let pseudo = pseudoInput.value;
+    if(pseudo.length < 3 || pseudo.length > 20){
+        pseudoInput.classList.add('is-invalid');
+        return false;
+    } else {
+        pseudoInput.classList.remove('is-invalid');
+        pseudoInput.classList.add('is-valid');
+        return true;
+    }
+}
+
+avisInput.addEventListener('input', validateForm);
+pseudoInput.addEventListener('input', validateForm);
+
+function validateForm(){
+    if (validationLenPseudo() && validationLenAvis_Content() && validationNote()){
+        btnSendReview.classList.remove('disabled');
+    }else{
+        btnSendReview.classList.add('disabled');
+    }
+}
+
 
 
 /*************************************************************************/
--- src/Form/AvisType.php
+++ src/Form/AvisType.php
@@ -4,11 +4,14 @@ namespace App\Form;
 
 use App\Entity\Avis;
 use App\Entity\Zoo;
+use Doctrine\ODM\MongoDB\Aggregation\Stage\Search\Text;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AvisType extends AbstractType
 {
@@ -16,7 +19,15 @@ class AvisType extends AbstractType
     {
         $builder
             ->add('pseudo')
-            ->add('Avis_content')
+            ->add('Avis_content', TextAreaType::class, [
+                'attr' => [
+                    'placeholder' => 'Votre avis',
+                    'class' => 'form-control',
+                    'rows' => '5',
+                    'cols' => '33',
+                    'maxlength' => '255'
+                ],
+            ])
             ->add('note', RangeType::class, [
                 'attr' => [
                     'min' => 1,
