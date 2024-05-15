d5e0eb67591856f9df475b5685ce4ec163898b0d
Sebastien35 <Sborgne@live.fr>
2024-05-15 16:06:16 +0200

    add test for accueil

---
+++ tests/AcceuilControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AcceuilControllerTest extends WebTestCase
+{
+    public function testAcceuil(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Arcadia');
+    }
+}
