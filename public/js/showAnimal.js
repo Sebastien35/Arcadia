faccae9e39a1d4b8f68e7c6478e9dc2b5857523a
Sebastien35 <Sborgne@live.fr>
2024-05-27 14:47:26 +0200

    chore: Update showAnimal.js to add date filtering functionality

--- public/js/showAnimal.js
+++ public/js/showAnimal.js
@@ -1,3 +1,5 @@
+alert('showAnimal.js chargé');
+
 /*-----------------------------------------------------------------------------------*/
 /*---------------------Affichage des informations de l'animal------------------------*/
 /*-----------------------------------------------------------------------------------*/
@@ -11,7 +13,7 @@ const infoContainer = document.getElementById('infoContainer');
 //     FlushActive();
 //     getNonAdminUsers() 
 //     userContainer.classList.remove('d-none');
-//     button.classList.add('active'); // Use 'button' instead of 'userBtn' because 'button' is the current button being clicked
+//     button.classList.add('active'); 
 // }));
 
 infoBtn.forEach(button=>button.addEventListener('click', function(){
@@ -23,60 +25,59 @@ infoBtn.forEach(button=>button.addEventListener('click', function(){
 
 
 /*-----------------------Trier informations -----------------------------------*/
-document.addEventListener('DOMContentLoaded', function() {
-const dateInput = document.querySelectorAll('.dateInput');
-const repas = document.querySelectorAll('.repas');
-const repasDate = document.querySelectorAll('.repasdate');
+    const dateInput = document.querySelectorAll('.dateInput');
+    const repas = document.querySelectorAll('.repas');
+    const repasDate = document.querySelectorAll('.repasdate');
 
-if (dateInput) {
-dateInput.forEach(button=>button.addEventListener('click', function(){
-    // Quelle option a été sélectionnée ?
-    let selectedOption = parseInt(dateInput.value);
-    // Quelle est la date d'aujourd'hui ?
-    let today = new Date();
-    // Quelle est le premier jour de la semaine ?
-    let weekStart = new Date();
-    // Quel jour de la semaine est aujourd'hui ?
-    weekStart.setDate(today.getDate() - today.getDay()); 
-    // Quelle est le premier jour du mois ?
-    let monthStart = new Date(today.getFullYear(), today.getMonth(), 1); 
-    
 
-    repas.forEach((repasItem) => {
-        // Obtenir la date du repas
-        let repasDateStr = repasItem.querySelector('.repasdate').textContent;
-        let repasDateTime = new Date(repasDateStr);
+    dateInput.forEach(button=>button.addEventListener('change', function(event){
         
-        switch (selectedOption) {
-            case 1: // Aujourd'hui
-                if (isSameDay(repasDateTime, today)) {
+        // Quelle option a été sélectionnée ?
+        let selectedOption = parseInt(event.target.value);
+        console.log(selectedOption);
+        // Quelle est la date d'aujourd'hui ?
+        let today = new Date();
+        // Quelle est le premier jour de la semaine ?
+        let weekStart = new Date();
+        // Quel jour de la semaine est aujourd'hui ?
+        weekStart.setDate(today.getDate() - today.getDay()); 
+        // Quelle est le premier jour du mois ?
+        let monthStart = new Date(today.getFullYear(), today.getMonth(), 1); 
+        
+
+        repas.forEach((repasItem) => {
+            // Obtenir la date du repas
+            let repasDateStr = repasItem.querySelector('.repasdate').textContent;
+            let repasDateTime = new Date(repasDateStr);
+            
+            switch (selectedOption) {
+                case 1: // Aujourd'hui
+                    if (isSameDay(repasDateTime, today)) {
+                        repasItem.classList.remove('d-none');
+                    } else {
+                        repasItem.classList.add('d-none');
+                    }
+                    break;
+                case 2: // Cette semaine
+                    if (repasDateTime >= weekStart && repasDateTime <= today) {
+                        repasItem.classList.remove('d-none');
+                    } else {
+                        repasItem.classList.add('d-none');
+                    }
+                    break;
+                case 3: // Ce mois
+                    if (repasDateTime.getFullYear() === today.getFullYear() &&
+                        repasDateTime.getMonth() === today.getMonth()) {
+                        repasItem.classList.remove('d-none');
+                    } else {
+                        repasItem.classList.add('d-none');
+                    }
+                    break;
+                default: // Tous les repas
                     repasItem.classList.remove('d-none');
-                } else {
-                    repasItem.classList.add('d-none');
-                }
-                break;
-            case 2: // Cette semaine
-                if (repasDateTime >= weekStart && repasDateTime <= today) {
-                    repasItem.classList.remove('d-none');
-                } else {
-                    repasItem.classList.add('d-none');
-                }
-                break;
-            case 3: // Ce mois
-                if (repasDateTime.getFullYear() === today.getFullYear() &&
-                    repasDateTime.getMonth() === today.getMonth()) {
-                    repasItem.classList.remove('d-none');
-                } else {
-                    repasItem.classList.add('d-none');
-                }
-                break;
-            default: // Tous les repas
-                repasItem.classList.remove('d-none');
-        }
-    });
-}));
-}   
-});
+            }
+        });
+    }));
 
 function isSameDay(date1, date2) {
     return date1.getFullYear() === date2.getFullYear() &&
